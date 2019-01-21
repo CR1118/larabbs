8f41e72ec60898ab8f9ff76256a775f035cb3b83
CR <xiaotourui@163.com>
2019-01-21 16:51:06 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+// 用户
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
